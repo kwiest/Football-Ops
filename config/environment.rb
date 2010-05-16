e32a8b7aa949ce4185ec71e2e6a27c0fd6941641
Kyle Wiest <kyle.wiest@gmail.com>
2010-05-15 22:52:53 -0700

    Removed s3 and paperclip gems

--- config/environment.rb
+++ config/environment.rb
@@ -5,10 +5,8 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   
   config.gem 'authlogic'
-  config.gem 'paperclip'
   config.gem 'formtastic'
   config.gem 'canable'
-  config.gem 'aws-s3'
   
   config.time_zone = 'Pacific Time (US & Canada)'
 end
