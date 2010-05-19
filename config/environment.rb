970e0e35cec5deba2555655dd851c41528e226ff
Kyle Wiest <kyle.wiest@gmail.com>
2010-05-18 21:53:22 -0700

    Added inherited_resources, responders, and will_paginate gem

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,11 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   
-  config.gem 'authlogic'
-  config.gem 'formtastic'
+  config.gem 'authlogic', :version => '2.1.3'
+  config.gem 'formtastic', :version => '0.9.8'
+  config.gem 'inherited_resources', :version => '1.0.6'
+  config.gem 'responders', :version => '0.4.7'
+  config.gem 'will_paginate', :version => '2.3.12'
   
   config.time_zone = 'Pacific Time (US & Canada)'
 end
