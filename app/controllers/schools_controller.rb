40211fea79d696ff6a4e6be16edb6c43d3b31658
Kyle Wiest <kyle.wiest@gmail.com>
2010-05-20 13:06:39 -0700

    Abandoned Google Maps display (frivilous), and also removed old jrails plugin

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,19 +1,7 @@
 class SchoolsController < InheritedResources::Base
   before_filter :sign_in_required
   actions :index, :show
-  respond_to :html, :xml, :json, :rb
-  
-  def show
-    @school = School.find(params[:id])
-    @map = GMap.new("map_div")
-    @coordinates = @school.geocoded_location.results[0].coordinates
-    @map.control_init(:small => true)
-    @map.center_zoom([@coordinates[0], @coordinates[1]], 14)
-    @map.overlay_init(GMarker.new(
-      [@coordinates[0], @coordinates[1]], 
-      :title => @school.name, 
-      :info_bubble => @school.full_address))
-  end
+  respond_to :html, :xml, :json
   
   def search
     @schools = School.name_like(params[:name]).paginate(:page => params[:page], :per_page => 30)
--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,6 @@ Rails::Initializer.run do |config|
   config.gem 'inherited_resources', :version => '1.0.6'
   config.gem 'will_paginate', :version => '2.3.12'
   config.gem 'searchlogic', :version => '2.4.19'
-  config.gem 'google_geocoding', :version => '0.2.0'
   
   config.time_zone = 'Pacific Time (US & Canada)'
 end
