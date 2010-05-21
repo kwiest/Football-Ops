af32655568733303d0cad3a4576d3ec151d88286
Kyle Wiest <kyle.wiest@gmail.com>
2010-05-21 10:04:20 -0700

    Now using Sendgrid for password reset emails (no more than 200 per day)

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -5,4 +5,13 @@ class UserMailer < MadMimiMailer
     from        "do-not-reply@footballops.org"
     body        :user => user
   end
+
+  def password_reset_instructions(user)
+    recipients      user.email
+    from            "do-not-reply@footballops.org"
+    subject         "FootballOps Password Reset Instructions"
+    sent_on         Time.now
+    body            :user => user
+    content_type    "text/html"
+  end
 end
