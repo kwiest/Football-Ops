027e11ca2177a7cb33875ad2578cb90fe3fca5a2
Kyle Wiest <kyle.wiest@gmail.com>
2010-05-21 15:38:05 -0700

    Fixed paperclip validation

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,7 +6,7 @@ class Upload < ActiveRecord::Base
 
   validates_presence_of :name
   validates_attachment_presence :asset
-  validates_attachment_file_size :asset, :less_than => 5.megabytes
+  validates_attachment_size :asset, :less_than => 5.megabytes
   validates_attachment_content_type :asset,
     :content_type => ["image/jpeg", "image/png", "application/pdf"],
     :message => "Files can only be images (jpg, jpeg, png), or PDFs"
