88347390b60bf9e1335f49ab0c0c45a72027b9ae
Kyle Wiest <kyle.wiest@gmail.com>
2011-09-07 14:57:12 -0700

    S3 credentials in upload model

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -6,7 +6,11 @@ class Upload < ActiveRecord::Base
   
   has_attached_file :asset,
     storage: :s3,
-    s3_credentials: "#{Rails.root}/config/amazon_s3.yml",
+    s3_credentials: {
+      access_key_id: ENV['S3_KEY'],
+      secret_access_key: ENV['S3_SECRET']
+    },
+    bucket: ENV['S3_BUCKET'],
     path: ":attachment/:id/:filename"
   validates_attachment_presence :asset, message: "Can't be blank"
   validates_attachment_content_type :asset,
