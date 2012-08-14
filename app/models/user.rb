1c4c7d8ad36a7bea0430b8a8d497a590678c2ddd
Kyle Wiest <kyle.wiest@gmail.com>
2012-08-13 17:25:46 -0700

    Remove bad newsletter callbacks

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,42 +21,8 @@ class User < ActiveRecord::Base
   
   default_scope order: :last_name, include: [:school, :division, :conference, :district]
   
-  after_create :subscribe_to_newsletter!
-  before_update :check_subscription_status
-  before_destroy :unsubscribe_from_newsletter!
-  
   def full_name
     [first_name, last_name].join(" ")
   end
   alias_method :name, :full_name
-  
-  def subscribe_to_newsletter!
-    HOMINID.subscribe(newsletter_list_id, email, { :FNAME => first_name, :LNAME => last_name }, { :email_type => 'html' })
-  end
-  
-  def unsubscribe_from_newsletter!
-    HOMINID.unsubscribe(newsletter_list_id, email) if subscribed_to_newsletter?
-  end
-  
-  def subscribed_to_newsletter?
-    info = HOMINID.member_info(newsletter_list_id, email)
-    info["status"] == "subscribed"
-  rescue Hominid::ListError
-    false
-  end
-  
-  
-  private
-  
-  def newsletter_list_id
-    "de1f0cd8b5"
-  end
-  
-  def check_subscription_status
-    if email_changed?
-      subscribe_to_newsletter! unless subscribed_to_newsletter?
-    else
-      true
-    end
-  end
 end
