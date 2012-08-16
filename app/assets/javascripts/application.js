52dc197f9c288b3e95457d6e6bcd88a797339c98
Kyle Wiest <kyle.wiest@gmail.com>
2012-08-15 19:48:51 -0700

    Install batman-rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,23 @@
 //= require modernizr-1.6.min
 //= require twitter/bootstrap
 //= require_self
+
+// Batman.js and its adapters
+//= require batman/es5-shim
+//= require batman/batman
+//= require batman/batman.jquery
+//= require batman/batman.rails
+
+//= require football_ops
+
+//= require_tree ./models
+//= require_tree ./controllers
+//= require_tree ./helpers
+
+
 //= require_tree .
+// Run the Batman app
+$(document).ready(function(){
+  FootballOps.run();
+});
+
