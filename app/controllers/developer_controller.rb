0e6c5f54fb9b9528c6f37f9331390343f3bfd352
Kyle Wiest <kyle.wiest@gmail.com>
2012-08-17 18:52:32 -0700

    Use the current_user to display live data for API docs

--- app/controllers/developer_controller.rb
+++ app/controllers/developer_controller.rb
@@ -1,8 +1,14 @@
 class DeveloperController < ApplicationController
+  skip_before_filter :ensure_authenticated
 
   def users
-    @user = UserSerializer.new(User.find 105).serializable_hash
+    @kyle= UserSerializer.new(User.find 105).serializable_hash
     @hawk = UserSerializer.new(User.find 104).serializable_hash
+    if signed_in?
+      @user = UserSerializer.new(current_user).serializable_hash
+    else
+      @user = @kyle
+    end
   end
 
 end
