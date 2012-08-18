cb70c07f130d2c7d9c534bc6fc5a0d0a70902fe4
Kyle Wiest <kyle.wiest@gmail.com>
2012-08-17 17:00:28 -0700

    Remove old methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,58 +1,15 @@
 class UsersController < ApplicationController
+  skip_before_filter :ensure_authenticated
   load_and_authorize_resource except: [:index, :conference_reps, :national_committee, :search]
-  
-  respond_to :html, :json
-  
-  def index
-    @users = User.paginate(page: params[:page], per_page: 30)
-  end
-  
-  def show
-  end
   
   def new
   end
-  
-  def edit
-  end
-  
+
   def create
     if @user.save
       redirect_to user_path(@user), notice: "User successfully created!"
     else
       render action: :new
     end
-  end
-  
-  def update
-    if @user.update_attributes(params[:user])
-      redirect_to user_path(@user), notice: "User successfully updated!"
-    else
-      render action: :edit
-    end
-  end
-  
-  def destroy
-    @user.destroy
-    redirect_to users_path, alert: "User successfully deleted."
-  end
-  
-  # Non RESTful Actions
-  
-  def conference_reps
-    @users = User.where(conference_rep: true).paginate(:page => params[:page], :per_page => 30)
-    authorize! :read, @users
-  end
-  
-  def national_committee
-    @users = User.where(national_committee: true).paginate(:page => params[:page], :per_page => 30)
-    authorize! :read, @users
-  end
-  
-  def search
-    @query = params.fetch :q, ''
-    @users = User.search(@query).paginate(page: params[:page], per_page: 50)
-    authorize! :read, @users
-    render action: :index
   end
 end
