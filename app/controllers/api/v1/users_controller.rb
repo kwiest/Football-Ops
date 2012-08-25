9d10518fb8a836d47e419e2308ade9cc83e8b96c
Kyle Wiest <kyle.wiest@gmail.com>
2012-08-25 14:58:50 -0700

    Use same naming in all pagination areas

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,10 @@ module Api
       load_and_authorize_resource
 
       def index
-        page     = params.fetch :page, 1
-        per_page = params.fetch :per_page, 25
+        page  = params.fetch :page, 1
+        limit = params.fetch :limit, 25
 
-        @users = User.page(page).per(per_page)
+        @users = User.page(page).per(limit)
         render json: @users.to_a, root: false
       end
 
