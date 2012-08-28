2a6761fc7e61b493cf89f6a1d0b6c4f54825488a
Kyle Wiest <kyle.wiest@gmail.com>
2012-08-27 18:28:26 -0700

    Use #should as a class-level method

--- test/unit/users_test.rb
+++ test/unit/users_test.rb
@@ -5,19 +5,17 @@ class UserTest < ActiveSupport::TestCase
     @kyle = users :kyle
   end
 
-  def test_validations
-    assert validate_presence_of(:first_name)
-    assert validate_presence_of(:last_name)
-    assert validate_presence_of(:email)
-    assert validate_presence_of(:school)
-  end
+  # Test validations
+  should validate_presence_of(:first_name)
+  should validate_presence_of(:last_name)
+  should validate_presence_of(:email)
+  should validate_presence_of(:school)
 
-  def test_associations
-    assert belong_to :school
-    assert have_one :conference
-    assert have_one :district
-    assert have_one :division
-  end
+  # Test associations
+  should belong_to :school
+  should have_one :conference
+  should have_one :district
+  should have_one :division
 
   def test_full_name
     assert_equal 'Kyle Wiest', @kyle.full_name
