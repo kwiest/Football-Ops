c2ed6aa0297b3018a0f369618abac8107834e3aa
Kyle Wiest <kyle.wiest@gmail.com>
2012-08-27 18:33:16 -0700

    Remove unused methods in and unit test Division

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,13 +1,7 @@
 class Division < ActiveRecord::Base  
   has_many :schools
   has_many :users, through: :schools
-  has_many :uploads, as: :uploadable
   
   validates_presence_of :name
   default_scope order: :name
-  
-  def to_param
-    "#{id}-#{name.parameterize}"
-  end
-  
 end
