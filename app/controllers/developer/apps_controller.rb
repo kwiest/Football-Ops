d19cc6664d8948680d4ac6083416eb4c7fcef9cf
Kyle Wiest <kyle.wiest@gmail.com>
2012-09-20 15:06:44 -0700

    Redirect to properly namespaced route

--- app/controllers/developer/apps_controller.rb
+++ app/controllers/developer/apps_controller.rb
@@ -20,7 +20,7 @@ module Developer
     def create
       @app = current_user.apps.create params[:app]
       if @app.save
-        redirect_to apps_path(@app), notice: 'App successfully created.'
+        redirect_to developer_app_path(@app), notice: 'App successfully created.'
       else
         render action: 'new'
       end
@@ -28,7 +28,7 @@ module Developer
 
     def update
       if @app.update_attributes params[:app]
-        redirect_to apps_path(@app), notice: 'App successfully updated.'
+        redirect_to developer_app_path(@app), notice: 'App successfully updated.'
       else
         render action: 'edit'
       end
@@ -36,7 +36,7 @@ module Developer
 
     def destroy
       @app.destroy
-      redirect_to apps_path, alert: 'App destroyed.'
+      redirect_to developer_apps_path, alert: 'App destroyed.'
     end
 
 
@@ -45,7 +45,7 @@ module Developer
     def assign_app
       @app = current_user.apps.find params[:id]
     rescue ActiveRecord::RecordNotFound
-      redirect_to apps_path, alert: 'Sorry, could not find your app.'
+      redirect_to developer_apps_path, alert: 'Sorry, could not find your app.'
     end
 
     def ensure_authenticated
