78336db751525e673b4ce7abd9122061c30280e6
Kyle Wiest <kyle.wiest@gmail.com>
2012-10-02 16:20:17 -0700

    Generate token on create and validate presence of app and user records.

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,4 +1,16 @@
 class AccessToken < ActiveRecord::Base
   belongs_to :app
   belongs_to :user
+
+  before_create :generate_token
+
+  validates :app, :user, presence: true
+
+  private
+
+  def generate_token
+    begin
+      self.token = SecureRandom.hex 32
+    end while self.class.exists? token: token
+  end
 end
--- test/unit/access_token_test.rb
+++ test/unit/access_token_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class AccessTokenTest < ActiveSupport::TestCase
+  # Test validations
+  should validate_presence_of :app
+  should validate_presence_of :user
+
   # Test associations
   should belong_to :app
   should belong_to :user
+
+  def test_generate_token_on_create
+    app = apps :ios
+    user = users :kyle
+    access_token = AccessToken.create! app: app, user: user
+
+    refute_nil access_token.token
+  end
 end
