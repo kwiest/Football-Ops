de42dc135277910b4af871e1eb91b9e389127b9d
Kyle Wiest <kyle.wiest@gmail.com>
2012-10-02 12:01:42 -0700

    Render a page to allow users to authorize apps, and have controller redirect with response

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,9 +1,32 @@
 class OauthController < ApplicationController
   before_filter :ensure_authenticated
+  before_filter :find_app_by_api_key, only: %w(authorize create_authorization_code)
 
   def authorize
-    @app          = App.find params[:api_key]
     @redirect_uri = params.fetch :redirect_uri, @app.redirect_uri
     @state        = params.fetch :state, ''
+  end
+
+  def create_authorization_code
+    app_redirect_uri = params[:redirect_uri]
+    state            = params[:state]
+
+    if params[:authorize]
+      authorization_code = @app.create_authorization_code!
+      redirect_uri = "#{app_redirect_uri}?authorization_code=#{authorization_code}&state=#{state}"
+    else
+      redirect_uri = "#{app_redirect_uri}?authorization_code=DENIED&state=#{state}"
+    end
+
+    redirect_to redirect_uri
+  end
+
+
+  private
+
+  def find_app_by_api_key
+    @app = App.find_by_api_key! params[:api_key]
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: 'Sorry, but we could not find that application'
   end
 end
--- test/integration/oauth_flow_test.rb
+++ test/integration/oauth_flow_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class OauthFlowTest < ActionDispatch::IntegrationTest
   def setup
-    @kyle = users :kyle
+    @kyle = users :kyle; @kyle.password = 'secret'
     @app  = apps :ios
     sign_in @kyle
   end
 
   def test_authorizing_an_app
-    visit '/oauth/authorize'
+    visit "/oauth/authorize?api_key=#{@app.api_key}"
     @app.expects :create_authorization_code
     click_button 'Allow'
     assert_redirected_to @app.redirect_uri
