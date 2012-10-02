abc2076b0c464a0be4f89151c2417173d170f6de
Kyle Wiest <kyle.wiest@gmail.com>
2012-10-02 15:27:15 -0700

    Remove period since minitest inserts it automatically

--- test/support/assertion_helpers.rb
+++ test/support/assertion_helpers.rb
@@ -3,12 +3,12 @@ module AssertionHelpers
   def assert_change(object)
     old = object.call
     yield
-    refute_equal old, object.call, 'Object should have changed, but did not.'
+    refute_equal old, object.call, 'Object should have changed, but did not'
   end
 
   def refute_change(object)
     old = object.call
     yield
-    assert_equal old, object.call, 'Object should not have changed, but did.'
+    assert_equal old, object.call, 'Object should not have changed, but did'
   end
 end
