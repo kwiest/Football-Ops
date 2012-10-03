1f55942487805d64f6d8d00f6ad8890830950fc5
Kyle Wiest <kyle.wiest@gmail.com>
2012-10-02 18:17:08 -0700

    Enable creation of AccessTokens from an authorization code

--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -1,3 +1,5 @@
+class AuthorizationCodeExpiredError < RuntimeError; end
+
 class AccessToken < ActiveRecord::Base
   belongs_to :app
   belongs_to :user
@@ -6,11 +8,24 @@ class AccessToken < ActiveRecord::Base
 
   validates :app, :user, presence: true
 
+  def self.create_from_authorization_code(authorization_code)
+    raise AuthorizationCodeExpiredError if authorization_code.expired?
+    app, user  = authorization_code.app, authorization_code.user
+
+    destroy_old_tokens app, user
+    create! app: app, user: user
+  end
+
+
   private
 
   def generate_token
     begin
       self.token = SecureRandom.hex 32
     end while self.class.exists? token: token
+  end
+
+  def self.destroy_old_tokens(app, user)
+    where(app_id: app, user_id: user).map(&:destroy) 
   end
 end
--- test/unit/access_token_test.rb
+++ test/unit/access_token_test.rb
@@ -9,11 +9,39 @@ class AccessTokenTest < ActiveSupport::TestCase
   should belong_to :app
   should belong_to :user
 
+  def setup
+    @app  = apps :ios
+    @user = users :kyle
+  end
+
   def test_generate_token_on_create
-    app = apps :ios
-    user = users :kyle
-    access_token = AccessToken.create! app: app, user: user
+    access_token = AccessToken.create! app: @app, user: @user
 
     refute_nil access_token.token
+  end
+
+  def test_destroy_old_tokens
+    4.times { AccessToken.create! app: @app, user: @user }
+    assert_equal 4, AccessToken.where(app_id: @app, user_id: @user).size
+
+    AccessToken.destroy_old_tokens @app, @user
+    assert_equal 0, AccessToken.where(app_id: @app, user_id: @user).size
+  end
+
+  def test_create_from_authorization_code
+    auth_code = @app.create_authorization_code_for_user @user
+
+    AccessToken.expects :destroy_old_tokens
+    access_token = AccessToken.create_from_authorization_code auth_code
+
+    assert_equal @app, access_token.app
+    assert_equal @user, access_token.user
+  end
+
+  def test_creating_with_an_expired_authorization_code
+    auth_code = stub expired?: true
+    assert_raise AuthorizationCodeExpiredError do
+      AccessToken.create_from_authorization_code auth_code
+    end
   end
 end
