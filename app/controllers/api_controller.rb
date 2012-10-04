7600dcbdc4a1230b9a52e02864a12972ac8fffef
Kyle Wiest <kyle.wiest@gmail.com>
2012-10-03 18:53:03 -0700

    Add API filters to ensure token auth

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,34 @@
 class ApiController < ApplicationController
-  before_filter :ensure_authenticated
+  before_filter :set_custom_response_headers
+  before_filter :authenticate_with_token
 
 
   private
 
-  def ensure_authenticated
-    head :unauthorized unless signed_in?
+  def set_custom_response_headers
+    if request.request_method == 'OPTIONS'
+      response.headers['Cache-Control'] = 'no-cache'
+    end
+    response.headers['Access-Control-Allow-Headers']   = '*, Content-Type, Authorization'
+    response.headers['Access-Control-Allow-Methods']   = 'GET, POST, PUT, OPTIONS'
+    response.headers['Access-Control-Allow-Origin']    = '*'
+    response.headers['Access-Control-Request-Method']  = '*'
+  end
+
+  def authenticate_with_token
+    unauthorized_response unless request.headers.has_key? 'HTTP_AUTHORIZATION'
+    token = request.headers['Authorization'].split(' ')[1]
+    access_token = AccessToken.find_by_token! token
+    @current_user = access_token.user
+  rescue ActiveRecord::RecordNotFound
+    unauthorized_response
+  end
+
+  def unauthorized_response
+    head :unauthorized
+  end
+
+  def current_user
+    @current_user ||= authenticate_with_token
   end
 end
