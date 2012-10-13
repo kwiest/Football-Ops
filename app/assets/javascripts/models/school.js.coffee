320b8234b8d67279f8f6807c74f80771c62fd139
Kyle Wiest <kyle.wiest@gmail.com>
2012-10-13 10:59:38 -0700

    Encode school addresses

--- app/assets/javascripts/models/school.js.coffee
+++ app/assets/javascripts/models/school.js.coffee
@@ -3,7 +3,8 @@ class FootballOps.School extends FootballOps.BaseModel
     @resourceName: 'school'
     @storageKey: 'api/v1/schools'
 
-    @encode 'id', 'name', 'conference_id', 'district_id', 'division_id'
+    @encode 'id', 'name', 'address_street', 'address_city_state_zip',
+        'conference_id', 'district_id', 'division_id'
 
     @encode 'users'
         encode: false
--- app/serializers/school_serializer.rb
+++ app/serializers/school_serializer.rb
@@ -1,3 +1,11 @@
 class SchoolSerializer < ActiveModel::Serializer
-  attributes :id, :name, :conference_id, :district_id, :division_id
+  attributes :id, :name, :address_street, :address_city_state_zip, :conference_id, :district_id, :division_id
+
+  def address_street
+    school.address1
+  end
+
+  def address_city_state_zip
+    "#{school.city}, #{school.state} #{school.zip}"
+  end
 end
